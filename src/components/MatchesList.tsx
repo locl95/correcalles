986bc6d1c02e48ff58908707e622895c70bc3e6a
max <max.marin@whads.com>
2024-10-29 16:59:48 +0100

    summonerresume

--- src/components/MatchesList.tsx
+++ src/components/MatchesList.tsx
@@ -19,22 +19,36 @@ const MatchesList: React.FC<{item: Ranked}> = ({ item }) => {
     assists += item.matches[i].assists;
   }
 
-  const champsPicked = item.matches.reduce<{[key: string]: number}>((acc, item) => {
-    const name = item.championName;
-    acc[name] = acc[name] ? acc[name] + 1 : 1;
+  const champsPicked = item.matches.reduce<{[key: string]: { count: number, wins: number, kills: number, assists: number, deaths: number}}>((acc, match) => {
+    const name = match.championName;
+    if (!acc[name]) {
+      acc[name] = { count: 0, wins: 0, kills: match.kills, assists: match.assists, deaths: match.deaths }
+    }
+    else {
+      acc[name].kills += match.kills;
+      acc[name].assists += match.assists;
+      acc[name].deaths += match.deaths;
+    }
+    if (match.win) acc[name].wins += 1;
+    acc[name].count += 1;
     return acc;
   }, {});
 
-  const resultArray = Object.entries(champsPicked)
-    .map(([name, count]) => ({
+  const SortedChampsPicked = Object.entries(champsPicked)
+    .map(([name, data]) => ({
       name: name,
-      timesAppeared: count
+      count: data.count,
+      wins: data.wins,
+      kills: data.kills,
+      assists: data.assists,
+      deaths: data.deaths,
     }))
-    .sort((a, b) => b.timesAppeared - a.timesAppeared);
+    .sort((a, b) => b.count - a.count);
 
   return (
     <div className="MatchesList"> 
       <div className="matchesResume">
+        <div>Last {num_matches} games </div>
         <div>
           <div>{num_matches}G {num_wins}V {num_loses}L</div>
           <Progress type="circle" percent={(Math.floor(num_wins/num_matches*100))} />
@@ -45,9 +59,10 @@ const MatchesList: React.FC<{item: Ranked}> = ({ item }) => {
           <div></div>
         </div>
         <div>
-          {resultArray.slice(0,3).map((entry) => (
-            <li key={entry.name}>
-              <img className="small-img" src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${entry.name}.png`}/> {entry.timesAppeared} times
+          {SortedChampsPicked.slice(0,3).map((champ) => (
+            <li key={champ.name}>
+              <img className="small-img" src={`https://ddragon.leagueoflegends.com/cdn/14.11.1/img/champion/${champ.name}.png`}/>
+              <div>{champ.wins/champ.count*100}% ({champ.wins}V {champ.count-champ.wins}L) <span className={((champ.kills+champ.assists)/champ.deaths)<3 ? `font-red` : `font-green`}>{((champ.kills+champ.assists)/champ.deaths).toFixed(2)}</span></div>
             </li>
           ))}
         </div>

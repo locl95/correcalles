c9329dc14ad573493d8f894a0c7018922a6fd7b2
max <max.marin@whads.com>
2024-10-30 19:37:25 +0100

    added sort list by diferent fields

--- src/components/SummonerList.tsx
+++ src/components/SummonerList.tsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Summoner } from '../App';
 import SummonerItem from './SummonerItem';
+import { CaretDownOutlined } from '@ant-design/icons';
 
 const SummonerList: React.FC<{data: Summoner[], type: string}> = ({ data, type }) => {
+  const [sortby, setSortby] = useState('tier');
+
   const ranks = ["CHALLENGER", "GRANDMASTER", "MASTER", "DIAMOND", "EMERALD", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON", "UNRANKED"];
   const divisions = ["I", "II", "III", "IV", "V"];
 
   data.sort((a, b) => {
     if (type === `FLEX`){
-      const tierDiff = ranks.indexOf(a.leagues.RANKED_FLEX_SR.tier) - ranks.indexOf(b.leagues.RANKED_FLEX_SR.tier);
-      if (tierDiff !== 0) return tierDiff;
-      const rankDiff = divisions.indexOf(a.leagues.RANKED_FLEX_SR.rank) - divisions.indexOf(b.leagues.RANKED_FLEX_SR.rank);
-      if (rankDiff !== 0) return rankDiff;
-      return a.leagues.RANKED_FLEX_SR.leaguePoints - b.leagues.RANKED_FLEX_SR.leaguePoints;
+      if (sortby === `summoner`) {
+        return a.summonerName.localeCompare(b.summonerName);
+      }
+      else if (sortby === `tier`) {
+        const tierDiff = ranks.indexOf(a.leagues.RANKED_FLEX_SR.tier) - ranks.indexOf(b.leagues.RANKED_FLEX_SR.tier);
+        if (tierDiff !== 0) return tierDiff;
+        const rankDiff = divisions.indexOf(a.leagues.RANKED_FLEX_SR.rank) - divisions.indexOf(b.leagues.RANKED_FLEX_SR.rank);
+        if (rankDiff !== 0) return rankDiff;
+        return a.leagues.RANKED_FLEX_SR.leaguePoints - b.leagues.RANKED_FLEX_SR.leaguePoints;
+      }
+      else if (sortby === `games`) {
+        return b.leagues.RANKED_FLEX_SR.gamesPlayed - a.leagues.RANKED_FLEX_SR.gamesPlayed;
+      }
+      else {
+        return b.leagues.RANKED_FLEX_SR.winrate - a.leagues.RANKED_FLEX_SR.winrate;
+      }
     }
     else {
-      const tierDiff = ranks.indexOf(a.leagues.RANKED_SOLO_5x5.tier) - ranks.indexOf(b.leagues.RANKED_SOLO_5x5.tier);
-      if (tierDiff !== 0) return tierDiff;
-      const rankDiff = divisions.indexOf(a.leagues.RANKED_SOLO_5x5.rank) - divisions.indexOf(b.leagues.RANKED_SOLO_5x5.rank);
-      if (rankDiff !== 0) return rankDiff;
-      return a.leagues.RANKED_SOLO_5x5.leaguePoints - b.leagues.RANKED_SOLO_5x5.leaguePoints;
+      if (sortby === `summoner`) {
+        return a.summonerName.localeCompare(b.summonerName);
+      }
+      else if (sortby === `tier`) {
+        const tierDiff = ranks.indexOf(a.leagues.RANKED_SOLO_5x5.tier) - ranks.indexOf(b.leagues.RANKED_SOLO_5x5.tier);
+        if (tierDiff !== 0) return tierDiff;
+        const rankDiff = divisions.indexOf(a.leagues.RANKED_SOLO_5x5.rank) - divisions.indexOf(b.leagues.RANKED_SOLO_5x5.rank);
+        if (rankDiff !== 0) return rankDiff;
+        return a.leagues.RANKED_SOLO_5x5.leaguePoints - b.leagues.RANKED_SOLO_5x5.leaguePoints;
+      }
+      else if (sortby === `games`) {
+        return b.leagues.RANKED_SOLO_5x5.gamesPlayed - a.leagues.RANKED_SOLO_5x5.gamesPlayed;
+      }
+      else {
+        return b.leagues.RANKED_SOLO_5x5.winrate - a.leagues.RANKED_SOLO_5x5.winrate;
+      }
     }
   });
 
   return (
     <div className="list"> 
       <div className="headrow turkish"> 
-        <div className="col">Summoner</div>
-        <div className="col">Rank</div>
-        <div className="col">LP</div>
-        <div className="col">Games</div>
-        <div className="col">Winrate</div>
+        <div className="col cursor-pointer" onClick={() => setSortby("summoner")}>Summoner {sortby === `summoner` && <CaretDownOutlined />}</div>
+        <div className="col cursor-pointer" onClick={() => setSortby("tier")}>Rank {sortby === `tier` && <CaretDownOutlined />}</div>
+        <div className="col cursor-pointer" onClick={() => setSortby("tier")}>LP</div>
+        <div className="col cursor-pointer" onClick={() => setSortby("games")}>Games {sortby === `games` && <CaretDownOutlined />}</div>
+        <div className="col cursor-pointer" onClick={() => setSortby("winrate")}>Winrate {sortby === `winrate` && <CaretDownOutlined />}</div>
       </div>
       {data.map((summoner) => {
          return (

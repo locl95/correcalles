767ce4afceadc1a3edd587db51100ac17ba4c353
max <max.marin@whads.com>
2024-11-07 21:57:54 +0100

    loading homepage

--- src/components/ViewsList.tsx
+++ src/components/ViewsList.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from '../pages/Home';
 import { useNavigate } from "react-router-dom";
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const ViewsList: React.FC<{list: View[], loading: boolean}> = ({ list, loading }) => {
   const navigate = useNavigate();
@@ -10,7 +12,8 @@ const ViewsList: React.FC<{list: View[], loading: boolean}> = ({ list, loading }
       <div className="headrow turkish"> 
         <div className="col cursor-pointer">Name</div>
       </div>
-      {list.map((view) => {
+      {loading && <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} /> }
+      {!loading && list.map((view) => {
         return (
           <div key={view.id} className="headrow blue border-5-blue" onClick={() => navigate(`/${view.id}`)}>
             <div className='col flex-start-lineal'>{view.name}</div>
